780cf864ada7b02daab815715cf306bfb7b1fc38
Vasil Kosturski <vasilkosturski@gmail.com>
2025-02-09 12:54:32 +0200

    Fixed tracing

--- services/order_service/app.py
+++ services/order_service/app.py
@@ -7,7 +7,7 @@ from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 from opentelemetry.instrumentation.kafka import KafkaInstrumentor
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -26,7 +26,7 @@ trace.set_tracer_provider(
         resource=Resource.create({SERVICE_NAME: "order_service"})
     )
 )
-otlp_exporter = OTLPSpanExporter(endpoint="localhost:4317", insecure=True)
+otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4318/v1/traces")
 trace.get_tracer_provider().add_span_processor(
     BatchSpanProcessor(otlp_exporter)
 )
