97e3b314893122c3d06a4db92a18c38c266b6fd3
Vasil Kosturski <vasilkosturski@gmail.com>
2025-03-28 11:40:17 +0200

    Refactor models to use records and Lombok annotations.

--- java/order-service/src/main/java/com/vkontech/orderservice/model/OrderDto.java
+++ java/order-service/src/main/java/com/vkontech/orderservice/model/OrderDto.java
@@ -1,7 +1,9 @@
 package com.vkontech.orderservice.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
 public class OrderDto {
     @JsonProperty("customer_id")
     private String customerId;
@@ -10,14 +12,4 @@ public class OrderDto {
     private String productId;
 
     private int quantity;
-
-    // Getters and setters (or use Lombok if preferred)
-    public String getCustomerId() { return customerId; }
-    public void setCustomerId(String customerId) { this.customerId = customerId; }
-
-    public String getProductId() { return productId; }
-    public void setProductId(String productId) { this.productId = productId; }
-
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
 }
